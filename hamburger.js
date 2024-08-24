d1cf0d54f82f6043d521a181dd0db54bf979e080
MeetPatel-2005 <23dcs085@charusat.edu.in>
2024-08-24 17:44:43 +0530

    Updated About Us Page

---
+++ hamburger.js
@@ -0,0 +1,7 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick= function() {
+    links = document.querySelector(".links");
+    links.classList.toggle("active");
+}
+hamburger = document.querySelector(".hamburger");
+    hamburger.classList.toggle("active");
